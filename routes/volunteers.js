bf68cd752a9d47b0c20e538ea77e3b74dfd15464
Rayeed _ <_rayeed@live.com>
2022-12-23 09:46:31 +0000

    add POST recipients and volunteers

---
+++ routes/volunteers.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET all volunteers. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+/* POST a new volunteer. */
+router.post("/", function (req, res, next) {
+  res.send({ ...req.body });
+});
+
+module.exports = router;
